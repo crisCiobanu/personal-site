4645b9854f2628b9edb92327f42e81464f1920b0
cristian <ciobanucristian.cc@gmail.com>
2023-02-05 22:11:04 +0100

    modules added

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainLayoutComponent
+  ],
+  exports: [
+    MainLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ]
+})
+export class CoreModule { }
---
+++ src/app/features/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mysite-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent {
+
+}
