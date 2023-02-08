bba9864f53e7170982baf7adfe12d9eb3c07b66b
c_ciobanu <ciobanucristian.cc@gmail.com>
2023-02-08 10:15:34 +0100

    switched to branch working

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    MainLayoutComponent
+    MainLayoutComponent,
+    HeaderComponent
   ],
   exports: [
     MainLayoutComponent
